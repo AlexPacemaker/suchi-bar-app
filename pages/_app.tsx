8193f7ec75bbdf93f8c38b8164a489b87f51e734
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-20 10:22:15 +0300

    upd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default App;
