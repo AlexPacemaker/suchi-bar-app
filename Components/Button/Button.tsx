17a4420c247cc482b48a7db6d5f65f531f8ce67f
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-20 15:24:01 +0300

    upd

--- Components/Button/Button.tsx
+++ Components/Button/Button.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import styles from './Button.module.scss'
 import { IButton } from "./Button.props";
+import cn from "classnames";
 
 const Button = ({ appearence, children }: IButton): JSX.Element => {
-  return <button>{children}</button>;
+  return <button className={styles.button}>{children}</button>;
 };
 
 export default Button;
