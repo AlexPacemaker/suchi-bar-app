c0bc5d848e77b1f1cbb076ed1ef33ab0838e0d13
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-20 17:29:24 +0300

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,16 @@ const Home = (): JSX.Element => {
       <Titles tag='h1'>Suchi Bonsai!</Titles>
       <Button appearence='primary'>Press Primary</Button>
       <Button appearence='ghost'>Press Ghost</Button>
-      <Paragraph>
+      <Paragraph size='s'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex,
         consectetur?
       </Paragraph>
-      <Paragraph>
+      <Paragraph size='m'>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta corrupti
         asperiores eligendi cumque quia beatae fugiat doloremque illum
         repudiandae temporibus.
       </Paragraph>
-      <Paragraph>
+      <Paragraph size='l'>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit
         tenetur ratione, molestias odio, quo dicta ipsam vero optio, accusamus
         perspiciatis! Sed distinctio eius quisquam, vel nam deserunt reiciendis
