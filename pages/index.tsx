4d997fd38f3ff9fcc6a7fd924ef7a3df8bc2f82d
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-21 14:37:20 +0300

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/Components/Button/Button";
 import Paragraph from "@/Components/Paragraph/Paragraph";
+import Tag from "@/Components/Tag/Tag";
 import Titles from "@/Components/Titles/Titles";
 import React from "react";
 
@@ -9,21 +10,27 @@ const Home = (): JSX.Element => {
       <Titles tag='h1'>Suchi Bonsai!</Titles>
       <Button appearence='primary'>Press Primary</Button>
       <Button appearence='ghost'>Press Ghost</Button>
-      <Paragraph size='s'>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex,
-        consectetur?
-      </Paragraph>
+      <Paragraph size='s'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex, consectetur?</Paragraph>
       <Paragraph size='m'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta corrupti
-        asperiores eligendi cumque quia beatae fugiat doloremque illum
-        repudiandae temporibus.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta corrupti asperiores eligendi cumque quia beatae
+        fugiat doloremque illum repudiandae temporibus.
       </Paragraph>
       <Paragraph size='l'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit
-        tenetur ratione, molestias odio, quo dicta ipsam vero optio, accusamus
-        perspiciatis! Sed distinctio eius quisquam, vel nam deserunt reiciendis
-        harum!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis impedit tenetur ratione, molestias odio, quo dicta
+        ipsam vero optio, accusamus perspiciatis! Sed distinctio eius quisquam, vel nam deserunt reiciendis harum!
       </Paragraph>
+      <Tag size='s' color='ghost'>
+        Ghost
+      </Tag>
+      <Tag size='m' color='red'>
+        Red
+      </Tag>
+      <Tag size='s' color='green'>
+        Green
+      </Tag>
+      <Tag color='primary' size='m'>
+        Green
+      </Tag>
     </>
   );
 };
