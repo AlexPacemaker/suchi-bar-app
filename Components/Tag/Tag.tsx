46861fe03bd7dc47eaa4e73e3d282a6cfe94fe9c
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-21 14:20:53 +0300

    tag added

---
+++ Components/Tag/Tag.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Tag.module.scss";
+import cn from "classnames";
+import { ITag } from "./Tag.props";
+
+const Tag = ({
+  children,
+  size = "s",
+  color = "ghost",
+  href,
+  className,
+  ...props
+}: ITag): JSX.Element => {
+  return (
+    <div
+      className={cn(styles.Tag, className, styles[size], styles[color])}
+      {...props}
+    >
+      {href ? <a href={href}>{children}</a> : <>{children}</>}
+    </div>
+  );
+};
+
+export default Tag;
