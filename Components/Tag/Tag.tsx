f01e8006138b4e56bcf219a1659f04f69c7edff4
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-22 14:35:46 +0300

    upd

--- Components/Tag/Tag.tsx
+++ Components/Tag/Tag.tsx
@@ -3,19 +3,9 @@ import styles from "./Tag.module.scss";
 import cn from "classnames";
 import { ITag } from "./Tag.props";
 
-const Tag = ({
-  children,
-  size = "s",
-  color = "ghost",
-  href,
-  className,
-  ...props
-}: ITag): JSX.Element => {
+const Tag = ({ children, size = "s", color = "ghost", href, className, ...props }: ITag): JSX.Element => {
   return (
-    <div
-      className={cn(styles.Tag, className, styles[size], styles[color])}
-      {...props}
-    >
+    <div className={cn(styles.Tag, className, styles[size], styles[color])} {...props}>
       {href ? <a href={href}>{children}</a> : <>{children}</>}
     </div>
   );
