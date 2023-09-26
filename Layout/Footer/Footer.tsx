be380c426d039166e2963884b1e38b3ed211a330
AlexPacemaker <118269766+AlexPacemaker@users.noreply.github.com>
2023-09-26 14:21:55 +0300

    upd

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,7 +1,18 @@
 import styles from "./Footer.module.scss";
 import cn from "classnames";
 import { IFooter } from "./Footer.props";
+import { format } from "date-fns";
 
-export const Footer = ({ ...props }: IFooter): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: IFooter): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Pacenaker © {format(new Date(), "yyyy")}</div>
+      <a href='#' target='_blank'>
+        Пользовательское соглашение
+      </a>
+      <a href='#' target='_blank'>
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
